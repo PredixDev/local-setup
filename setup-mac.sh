47f44bdcfb9082690a5f2a9b9359dd31d09e767e
predix-edgemanager-ui <predix-edgemanager-ui@ge.com>
2017-01-27 13:49:04 -0800

    update for Edgemanager UI specific changes

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,3 +1,5 @@
+
+
 #!/bin/bash
 set -e
 
@@ -9,6 +11,8 @@ sts=4
 nodejs=5
 python3=6
 uaac=7
+redis=8
+wct=9
 
 declare -a install
 
@@ -83,14 +87,17 @@ function brew_cask_install() {
 }
 
 function install_everything() {
+  # For front end environment Maven, Spring STS and Python are not required
   install[git]=1
   install[cf]=1
   install[jdk]=1
-  install[maven]=1
-  install[sts]=1
+  install[maven]=0
+  install[sts]=0
   install[nodejs]=1
-  install[python3]=1
+  install[python3]=0
   install[uaac]=0 # Install UAAC only if the --uaac flag is provided
+  install[redis]=1
+  install[wct]=1
 }
 
 function install_nothing() {
@@ -102,6 +109,8 @@ function install_nothing() {
   install[nodejs]=0
   install[python3]=0
   install[uaac]=0
+  install[redis]=0
+  install[wct]=0
 }
 
 function install_git() {
@@ -135,10 +144,14 @@ function install_maven() {
 }
 
 function install_nodejs() {
-  brew_install node
+  brew_install node 5.11.1
   node -v
   brew_install npm
   npm -v
+
+  npm set prefix “/usr/local”
+  npm set registry “http://registry.npmjs.org”
+  npm set strict-ssl false” 
 
   type bower > /dev/null || npm install -g bower
   echo -ne "\nbower "
@@ -146,6 +159,19 @@ function install_nodejs() {
 
   type grunt > /dev/null || npm install -g grunt-cli
   grunt --version
+}
+
+function install_redis() {
+  # Install Redis
+  brew_install redis
+  redis --version
+}
+
+function install_wct() {
+  # Install the Polymer web-components-tester
+  npm install -g https://github.com/Polymer/web-component-tester.git#v4.2.2
+  sudo chown -R $USER /usr/local
+  npm install web-component-tester-istanbul -g 
 }
 
 function install_python3() {
@@ -198,6 +224,8 @@ function run_setup() {
       [ "$1" == "--nodejs" ] && install[nodejs]=1
       [ "$1" == "--python3" ] && install[python3]=1
       [ "$1" == "--uaac" ] && install[uaac]=1
+      [ "$1" == "--redis" ] && install[redis]=1
+      [ "$1" == "--wct" ] && install[wct]=1
       shift
     done
   fi
@@ -228,6 +256,14 @@ function run_setup() {
 
   if [ ${install[nodejs]} -eq 1 ]; then
     install_nodejs
+  fi
+
+  if [ ${install[redis]} -eq 1 ]; then
+    install_redis
+  fi
+
+  if [ ${install[wct]} -eq 1 ]; then
+    install_wct
   fi
 
   if [ ${install[python3]} -eq 1 ]; then
